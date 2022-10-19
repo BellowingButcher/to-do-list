45fcb8943e0c215ff5ef4cb166759a6679b1bd28
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-19 23:03:54 +0000

    ???

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TaskInputSB from './sandbox/TaskInputSB';
 import AppSB from './sandbox/AppSB';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
