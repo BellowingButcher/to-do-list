b6b221f67decab944ec2bdc627cc7e8c2d1a2181
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-19 23:02:00 +0000

    I was working on local Storage
    - trying to get the count number in local storage to update along side the object id's
    - trying to get the count number on load to use the count number in localStorage

--- app/src/sandbox/AppSB.js
+++ app/src/sandbox/AppSB.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import TaskInputSB from "./TaskInputSB";
 function AppSB () {
+    const [keyArray, setKeyArray] = useState([]);
     const [taskArray, setTaskArray] = useState([]);
     const [page, setPage] = useState('all');
 
     return(
-        <TaskInputSB setTaskArray={setTaskArray} taskArray={taskArray}  />
+        <TaskInputSB setTaskArray={setTaskArray} taskArray={taskArray} keyArray={keyArray} setKeyArray={setKeyArray}  />
     )
 }
 export default AppSB
--- app/src/sandbox/TaskInputSB.js
+++ app/src/sandbox/TaskInputSB.js
@@ -1,36 +1,49 @@
 import React, { useState } from 'react';
 function TaskInputSB (props) {
     const [inputValue, setInputValue] = useState('');
-    // const [task, setTask] = useState([{text:"", active: Boolean, key: Number}]);
-    const [key, setKey] = useState(0);
-
+    const [count, setCount] = useState(Number);
+    function addToLocalStorage(key, temp, count){
+        
+        localStorage.setItem(key, JSON.stringify(temp));
+        
+    }
     function addTask(){
         let temp = {
             text: inputValue,
             active: true,
-            // id: key,
+            id: count,
         }
+        let taskNumber = localStorage.key.length;
         // setTask(temp);
-        // setKey(key+1);
         //add input value to an object boilerplate
         props.setTaskArray(
             [
                 ...props.taskArray,
                 temp
             ]
-        );
-        
+            );
+            localStorage.setItem('count', count);
+            addToLocalStorage(count, temp, taskNumber);
+            setCount(+1);
+        // props.setKeyArray(
+        //     [
+        //         ...props.keyArray,
+        //         key
+        //     ]
+        // )
+            
+            // setKey(key+1);
         //{text:'', active:true, id:number} number changes with each new list item
         //commit task to local storage
         
     }
     
-    console.log('taskArray at this point is', props.taskArray);
+    console.log('taskArray at this point', props.taskArray);
     // console.log('key at this point', key)
 return(
     <>
-    <input onChange={(e)=> setInputValue(e.target.value)} type="text" placeholder='What do you need done?' />
-    <button type="submit" onClick={ addTask }>Set Task</button>
+    <input onBlur={(e)=> setInputValue(e.target.value)} type="text" placeholder='What do you need done?' />
+    <button type="submit" onClick={()=>{addTask()} }>Set Task</button>
     </>
 )
 }
