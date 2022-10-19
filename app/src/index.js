7ecf8abe8a1ab7a71cf9a021381ca12147172b7e
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-19 16:57:08 +0000

    added empty files

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TaskInputSB from './sandbox/TaskInputSB';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
+  <TaskInputSB />
   </React.StrictMode>
 );
