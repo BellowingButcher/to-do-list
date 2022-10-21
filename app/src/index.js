4924bbe480abd824c00b7eb80dfaed2a330bf2fa
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-21 12:47:57 +0000

    changed comment

--- app/src/index.js
+++ app/src/index.js
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppBodySB2 />
-  {/* <AppSB /> */}
+    {/* <AppSB /> */}
   </React.StrictMode>
 );
