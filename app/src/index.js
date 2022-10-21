57a6e36bff14605d88ad20536bb3bfd89441543e
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-21 02:45:37 +0000

    idk why this wasnt added with my last commit

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppSB from './sandbox/AppSB';
+import AppBodySB2 from './sandbox2/AppBodySB2';
+// import AppSB from './sandbox/AppSB';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <AppSB />
+    <AppBodySB2 />
+  {/* <AppSB /> */}
   </React.StrictMode>
 );
