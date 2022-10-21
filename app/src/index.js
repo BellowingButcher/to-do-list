be6906c2bf49e78c1e3e1a363e9d380aceff1ddb
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-21 03:51:05 +0000

    this is demo day ready for as far as i can get
    -  I may be able to add more stuff but for tonight. Its a wrap

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppBodySB2 from './sandbox2/AppBodySB2';
-// import AppSB from './sandbox/AppSB';
+import AppSB from './sandbox/AppSB';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
