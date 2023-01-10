86169b70d6c6ea47620cb008e3c43ec76f3918fe
BellowingButcher <mason.williams@student.awesomeinc.org>
2023-01-10 23:16:08 +0000

    Header component styled and ready

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="container border border-5 rounded-pill">
+      <div className="row text-center">
+        <div className="col h1">To-Do</div>
+      </div>
+      <div className="row text-center">
+        <div className="col h1">ToDay</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
+
